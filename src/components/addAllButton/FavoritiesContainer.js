c4cf1e4320885e88486254f33e570ed3e578ca81
AidaMkrtchyan <aida.mkrtchyan0606@gmail.com>
2024-01-28 12:30:12 +0400

    Update FavoritiesContainer.js

--- src/components/addAllButton/FavoritiesContainer.js
+++ src/components/addAllButton/FavoritiesContainer.js
@@ -1,52 +1,47 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import "./favoritiesContainer.css";
-import songData from "../../data/song.json"
+import songData from "../../data/song.json";
 
-export default function FavoritiesContainer ({ song }) {
-  const [show,setShow] = useState(false);
-  const [songs, setSongs] = useState([]); 
-
-
-  
+export default function FavoritiesContainer({ song }) {
+  const [show, setShow] = useState(false);
 
   const handleFavoritesClick = (id) => {
     console.log(song);
-    songData.map((item)=>{
-      setSongs(songData);
-      console.log(songData);
-      if(item.id==id){
-        setShow(!show)
+    songData.forEach((item) => {
+      if (item.id === id) {
+        setShow(!show);
       }
-    })
-
+    });
   };
 
- 
   return (
     <div className='options'>
-      <button onClick={()=>handleFavoritesClick(song.id)}>
-        <img src={song.buttons.but[0].src} alt="hfdjgh" />
+      <button onClick={() => handleFavoritesClick(song.id)}>
+        <img src={song.buttons.but[0].src} alt="heart" />
       </button>
-      {show?<div className='fav'>
-      <div className='fav-list'> 
-                <div key={song.id} className='fav-item'>
-                <button className="fav_btn" onClick={()=>handleFavoritesClick(song.id)} aria-label="open new window">
-                    <img className='fav_grey' src="../icons/close.png" alt="close" />
-                  </button>
-                  <div className='favorities-box'>
-                    <img src={song.links.images[0].url} className='fav-img' alt="video" />
-                  </div>
-                      <div className='favorities-box_2'>
-                      <div className='fav-name'>{song.name}</div>
-                      <div className='fav-author'>{song.author}</div>
-                      <audio controls className='fav-play_btn'>
-                        <source src={song.audioUrl} />
-                      </audio>
-                    </div>
-                  </div>
+      {show ? (
+        <div className='fav'>
+          <div className='fav-list'>
+            <div key={song.id} className='fav-item'>
+              <button className="fav_btn" onClick={() => handleFavoritesClick(song.id)} aria-label="open new window">
+                <img className='fav_grey' src="../icons/close.png" alt="close" />
+              </button>
+              <div className='favorities-box'>
+                <img src={song.links.images[0].url} className='fav-img' alt="video" />
               </div>
-      </div>:null}
+              <div className='favorities-box_2'>
+                <div className='fav-name'>{song.name}</div>
+                <div className='fav-author'>{song.author}</div>
+                <audio controls className='fav-play_btn'>
+                  <source src={song.audioUrl} />
+                </audio>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
+
 
