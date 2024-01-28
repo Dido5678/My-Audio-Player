caab54f1d8c3ef49dc77bab5e31623d6f6fb165c
AidaMkrtchyan <aida.mkrtchyan0606@gmail.com>
2024-01-28 12:39:36 +0400

    add new test  App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -16,12 +16,3 @@ test('renders learn react link', async () => {
 
 
 
-
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
