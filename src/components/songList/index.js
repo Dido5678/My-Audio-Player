05a2786bbc460e27ae2ca334c3321c044141c0bd
AidaMkrtchyan <aida.mkrtchyan0606@gmail.com>
2024-01-28 12:37:03 +0400

    Update index.js

--- src/components/songList/index.js
+++ src/components/songList/index.js
@@ -9,13 +9,9 @@ import FavoritiesContainer from '../addAllButton/FavoritiesContainer';
 
 export default function SongList() {
   const [songs, setSongs] = useState([]); 
-  const [openSongId, setOpenSongId] = useState("");
-
 
   useEffect(() => {
-
     setSongs(songData.slice(0, 5));
-  
   }, []);
  
 
@@ -24,7 +20,6 @@ export default function SongList() {
     doneElements.forEach(element => {
         element.style.color = "red";
     });
-
 
     // Prevent the default button behavior, which might trigger form submission or page reload
     event.preventDefault();
@@ -56,10 +51,6 @@ export default function SongList() {
       console.warn('Audio elements not found.');
     }
   };
-  const handleFavoritesClick = (song) => {
-    setOpenSongId((prevOpenSongId) => !prevOpenSongId);
-    console.log(openSongId); 
-};
 
   return (
     <>
@@ -94,7 +85,7 @@ export default function SongList() {
                   <img src={song.links.images[0].url} className='songList-img' alt="foto artists" />
                 </div>
                 <div className='songList-song_more-options'>
-                <FavoritiesContainer song={song}/>
+                  <FavoritiesContainer song={song}/>
                   <img className="songList-song_done" src={song.buttons.but[1].src} alt="done" />
                   <ShareButton song={song} />
                   <Link to={'/favorites'}> 
@@ -133,89 +124,5 @@ export default function SongList() {
 
 
 
-// import React, { useState, useEffect } from 'react'; 
-// import './index.css';
-// import songData from '../../data/song.json';
-// // import SongRow from './SongRow/index';
-// import FavoritesBtn from '../addAllButton/FavoritiesButton';
-// import PlayButton from '../playAllButton/PlayButton';
-// import ShareButton from '../addAllButton/ShareButton';
 
 
-// export default function SongList() {
-//     const [songs, setSongs] = useState([]);
-//     const [openSongId, setOpenSongId] = useState(null);
-   
-
-
-
-//     const handleButtonClick = (event) => {
-//       event.preventDefault();
-
-//       const songListRow = event.target.closest('.songList-row');
-//       const audioElement = songListRow.querySelector('.songList-song_audio');
-//       const coverAudioElement = document.querySelector('.cover-song_audio');
-  
-//       if (audioElement && coverAudioElement) {
-//         const sourceElement = audioElement.querySelector('source');
-//         if (sourceElement) {
-//           coverAudioElement.src = sourceElement.src;
-//           coverAudioElement.play()
-//         }
-//       } else {
-//         console.warn('Audio elements not found.');
-//       }
-//     };
-    
-
-//     useEffect(() => {
-//       setSongs(songData.slice(0, 8));
-//     }, [songData]);   
-//     return(
-//       <>
-//         <songList className='songList-container'>
-//            <thead className='songList-thead'>
-//               <tr className='songList-tr'>
-//                 <th className='songList-one'></th>
-//                 <th className='songList-song'>Song Name</th>
-//                 <th className='songList-artist'>Artist Name</th>
-//                 <th className='songList-track'>Track</th>
-//                 <th className='songList-img'>image</th>
-//                 <th className='songList-zero'></th>
-//               </tr>
-//            </thead>
-//            <tbody className='songList-block'>
-//               {songs.map((song) => (
-//                <React.Fragment key={song.id}>
-//                   <tr className='songList-row'>
-//                      <td className='songList-data_first'>
-//                         <img className='songList-squared' src={song.icons.img[0].src} alt="squared" />
-//                         <button onClick={handleButtonClick}>
-//                           <img src={song.icons.img[1].src}  alt="image"/>
-//                         </button>
-//                      </td>
-//                      <td className='songList-song_name'>{song.name}</td>
-//                      <td className='songList-song_author'>{song.author}</td>
-//                      <td className='songList-song_audio-track'>
-//                       {song.id}  
-//                         <audio className='songList-song_audio' style={{display: "none"}} controls>
-//                           <source src={song.audioUrl}/>
-//                         </audio>
-//                      </td>
-//                      <td className='songList-song_image'>
-//                         <img src={song.links.images[0].url} className='songList-img' alt="image" />
-//                      </td>
-//                      <td className='songList-song_more-options'>
-//                         <FavoritesBtn song={song} />
-//                         <img src={song.buttons.but[1].src} alt="image" />
-//                         <ShareButton song={song} />
-//                         <PlayButton song={song} />
-//                      </td>
-//                    </tr>
-//                </React.Fragment>
-//                ))}
-//           </tbody>
-//         </songList>
-//       </>
-//     );
-// };
